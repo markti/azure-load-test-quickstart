c6d43207efbfdcc74faa01c076e23a1a65aeb5fd
markti <markti@outlook.com>
2023-01-03 11:00:43 -0500

    setup load test

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_resource_group" "main" {
   location  = var.location
 }
 
-resource "azurerm_load_example" "main" {
+resource "azurerm_load_test" "main" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   name                = "loadtest-${local.suffix}"
